18a69ee974b384be945f93bbdd81b6d6bc853ee2
musalesanket17 <musalesanket17@gmail.com>
2023-01-21 10:38:08 +0530

    DSA Test1

---
+++ DSA Tests/index.c
@@ -0,0 +1,42 @@
+Result: Pass
+Marks: 30/30
+Percentage: 100.00 %
+Questions: 10
+Correct Answers: 10
+Attempted: 10
+
+
+
+1. How Many iterations will it takes to find 38 from an given array using binary search arr = {10,20,38,57,100}.
+
+ONE itration only.
+
+
+2. _____ algorithm follows divide and conquer strategy.
+
+Quike Sort And Merge Sort.
+
+
+3. What is the Worst case time complexity of bubble sort?
+
+O(N^2)
+
+
+4. What is the worst case time complexity of Linear search?
+
+
+O(N)
+
+
+
+5. Binary search is commonly known as a --------------- or a ----------------.
+
+
+
+6. Which is not true about selection sort?
+7. What is the average case time complexity of selectoin sort?
+8. What is time complexity of Binary search?
+9. State the following statement is true ro false
+Linear search is rarely used practically because other search algorithms such as the binary search algorithm significantly faster searching comparisons.
+10. In_____ sort largest element gets bouble out and then 2nd largest element gets bouble out to get ascending ordered .
+ANS => bubble Sort
