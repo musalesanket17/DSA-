0ede4f31165d1616d157a8a2e117cec09a3b28e3
musalesanket17 <musalesanket17@gmail.com>
2023-01-21 10:39:41 +0530

    DSA Test3

---
+++ DSA Tests/index2.c
@@ -0,0 +1,91 @@
+Result: Pass
+Marks: 24/30
+Percentage: 73.33 %
+Questions: 10
+Correct Answers: 8
+Attempted: 10
+
+
+
+1. _____ operation get the top data element of the stack, without removing it.
+  
+
+       1. pop()
+       2. push()
+       3. IsEmpty()
+     T->4. peek()
+
+2. _______ can be used to test a palindrome?
+
+     T->1. Stack
+       2. Heap
+       3. Tree
+       4. queue
+
+
+3. If a user tries to remove an element from empty stack condition arises it is called _________.
+
+
+      1. POP
+    T->2. Underflow
+      3. Overflow
+      4. All of the above
+
+
+4. Stack can be implemented using _________ and ________ ?
+
+      1. Array and Binary Tree
+      2. Linked List and Graph
+   T->3. Array and Linked List
+      4. Queue and Linked List
+
+
+5. In which data structure element is inserted at one end called Rear and deleted at other end called Front.
+
+      1. Stack
+   T->2. Queue
+      3. Both
+      4. Binary Tree
+
+
+6. ______elements can be inserted or deleted from both the ends but not in the middle.
+
+     1. Stack
+     2. Queue
+     3. Priority queue
+   T->4. Dequeue
+
+
+7. Array implementation of Stack is not dynamic, which of the following statements supports this argument?
+
+
+   T->1. Space allocation for array is fixed and cannot be changed during run-time
+      2. User unable to give the input for stack operations
+      3. A runtime exception halts execution
+      4. Improper program compilation
+
+
+8. A data structure pushes the data downwards and enables the users to access the latest pushed data is :
+
+       1. Linear Queue
+    T->2. Stack
+       3. Circular Queue
+       4. Array
+
+
+9. Circular queue is also known as.
+
+       1. Standard Buffer
+       2. Cyclic Buffer
+     T->3. Ring Buffer
+       4. Radius Buffer
+
+
+
+10. In Linear Queue add operation has ----- time complexity.
+
+       1. O(n)
+       2. O(n-1)
+     T->3. O(1)
+       4. O(n-1)/2
+       
