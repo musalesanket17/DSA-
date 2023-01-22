e290972b8a0a32d3bc2591351a0e14368c8c115c
musalesanket17 <musalesanket17@gmail.com>
2023-01-22 09:18:50 +0530

    DSA Test4

---
+++ DSA Tests/index3.c
@@ -0,0 +1,115 @@
+Result: Pass
+Marks: 21/30
+Percentage: 60.00 %
+Questions: 10
+Correct Answers: 7
+Attempted: 10
+
+
+
+
+1. Which of the following is not application of graph ?
+
+        1. Coloring of maps algorithms
+        2. Job scheduling
+        3. Web Page navigation
+    T-> 4. Redo / Undo  actions
+
+
+
+2. What is strictly binary tree?
+
+        1. Binary tree all the internal nodes have 2 childrens?
+        2. All the leaf nodes are at same level.
+    T-> 3. BOTH are TRUE
+        4. BOTH are FALSE
+
+
+
+
+3. ---------- graph in which the edges have a specific direction which originates from one vertex and culminate .
+   into another vertex.
+
+
+         1. Cyclic Graph
+         2. Undirected Graph
+      T->3. Directed Graph
+         4. Connected Graph
+
+
+
+4. ----------- traversal is used to get a prefix expression from an expression tree.
+
+        1. PostOrder
+     T->2. PreOrder
+        3. InOroder
+        4. All of the above
+
+
+
+
+5. What is a hash table?
+
+       1. A structure that maps values to keys
+    T->2. A structure that maps keys to values
+       3. A structure used for storage
+       4. A structure used to implement stack and queue
+
+
+6. What is true about binary tree?
+
+        1. A binary tree is a type of tree in which every node has either no child node or one child node or two child nodes.
+        2. A binary tree is an important class of a tree data structure in which a node can have at most two children.
+     T->3. Both A and B
+        4. None of above
+
+
+
+
+7. How to represent Adjancy Matrix in graph?
+
+     T->1. It is Matrix a[v][v]   where v is no of vertices.   a[i][j] = 1 if i and j are adjacent            = 0 otherwise
+        2. It is Matrix a[v][v]   where v is no of edges.   a[i][j] = 1 if i and j are not adjacent            = 0 otherwise
+        3. Both A and B are correct
+        4. None of ABOVE
+
+
+
+8. Assume that a structure for a Binary Search Tree exists. What does the following function do?
+int function(root)
+{
+current = root;
+while (current->left != NULL)
+{ 
+current = current->left;
+}
+return(current->data);
+}
+
+
+    T->1. Left most child of BST.
+       2. Right most child of BST.
+       3. subtrees of BST
+       4. None above mentioned
+
+
+
+
+
+9. A binary tree whose every node has either zero or two children is called
+
+
+    T->1. Complete binary tree
+       2. Binary search tree
+       3. Extended binary tree
+       4. None of above
+
+
+
+10. Which of the following operations can be performed on graph.
+
+
+       1. Traversal
+       2. Add elements to graph
+       3. Finding the path
+    T->4. All of the above.
